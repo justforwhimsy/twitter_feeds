b78e129f2d4ec503ee180367e23f6e77da2e89cc
justforwhimsy <pomoluese@gmail.com>
2017-08-08 22:50:00 -0700

    Added logic to check for state abbreviations, comparing to a CSV file. Also split redundancies into a method

--- collect_twitter_accounts.py
+++ collect_twitter_accounts.py
@@ -4,6 +4,13 @@ import tweepy
 import logging
 import time
 import json
+import csv
+
+# Create a dictionary of state names with their abbreviations. Used when checking user entered locations
+with open('state_abbrevs.csv', mode ='r') as states:
+	reader = csv.reader(states)
+	# adds a dictionary key of the full state name with the value of the state abbreviation. District of Columbia is included
+	state_abbrevs = {rows[0]:rows[1] for rows in reader}
 
 #credentials stored in a json file so I don't commit code with sensative data
 with open('credentials.json') as json_creds:
@@ -16,6 +23,7 @@ with open('credentials.json') as json_creds:
 	dbuser = d['db']['user']
 	password = d['db']['password']
 
+# Set the auth for Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_token)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -36,20 +44,8 @@ def main():
 
 	start_time = time.time()
 	while county:
-		#collect the rate limit statuses to ensure we don't perform too many calls in a given timespan
-		rate_status = api.rate_limit_status()
-		logging.debug(rate_status)
-		rate_limit = rate_status['resources']['application']['/application/rate_limit_status']['remaining']
-		search_limit = rate_status['resources']['users']['/users/search']['remaining']
-		logging.debug('Rate Limit = ' +str(rate_limit))
-		logging.debug('User search limit = ' + str(search_limit))
-		
-		#if we reached the rate limit, we want to wait sleep through the remainder of the 15 minute window
-		if rate_limit == 0 or search_limit == 0:
-			tts = interval - (time.time() - start_time)
-			logging.info('Call limit reached. Sleeping for ' + str(tts) + " seconds")
-			time.sleep(tts + 10)#adding an extra 10 seconds to ensure we don't start early
-			start_time = time.time()
+			
+		check_rate_limit(start_time)
 
 		#search for county results in twitter.
 		logging.info('Retrieving accounts associated with ' + county)
@@ -63,26 +59,68 @@ def main():
 				screen_name = user.screen_name
 				name = user.name
 				uid = user.id_str
-				### TODO Collect followers related to a page
 				followers = get_followers(uid)
+				check_rate_limit(start_time)
 				### Create method for verifyng the location of the feed we searched for and the followers
 				#location = get_location(user)
 				# for follower in followers:
 				location = user.location
+				#verify a location has been entered
 				if location != "":
-					count +=1
+					if ',' in location: #assuming this is in the format of City, State so split it 
+						try:
+							city, state = location.split(',')
+							if len(state) != 2:
+								state = get_abbrev(state)
+						except Exception as e:
+							logging.error(e)
+							logging.error("Failed to split location: " + location)
+					elif location == 2:
+						state = get_abbrev(location)
+					else:
+						state = get_abbrev(location)
+						logging.info("No state found in location")
+						count +=1
 					try:
 						logging.debug('Name: ' + name + ' UID: ' + uid + " Screen Name: " + screen_name + ' Location: ' + location)
 					except Exception as e:
-						logging.error("Something is wrong with the content retrieved for the feed")
 						logging.error(e)
+						logging.error("Something is wrong with the content retrieved for the feed")
+						
 		county = cur.fetchone()[0]
-	
+
+#collect the rate limit statuses to ensure we don't perform too many calls in a given timespan	
+def check_rate_limit(start_time):
+	rate_status = api.rate_limit_status()
+	logging.debug(rate_status)
+	### TODO update to Grep for a value of 0 in the entir return value. This should appear in the format "': 0"
+	rate_limit = rate_status['resources']['application']['/application/rate_limit_status']['remaining']
+	search_limit = rate_status['resources']['users']['/users/search']['remaining']
+	followers_limit = rate_status['resources']['followers']['/followers/ids']['remaining']
+	logging.debug('Rate Limit = ' +str(rate_limit))
+	logging.debug('User search limit = ' + str(search_limit))
+	#if we reached the rate limit, we want to wait sleep through the remainder of the 15 minute window
+	if rate_limit == 0 or search_limit == 0 or followers_limit == 0:
+		tts = interval - (time.time() - start_time)
+		logging.info('Call limit reached. Sleeping for ' + str(tts) + " seconds")
+		time.sleep(tts + 10)#adding an extra 10 seconds to ensure we don't start early
+		start_time = time.time()
+
 def get_location(user):
 	###TODO
 	# parse out location if they have one - This is entered by the user so can vary 
 	# check location of tweets if that is enabled
 	# 			
+	return None
+def get_abbrev(state):
+	try:
+		logging.info("Collecting state abbreviation for " + state)
+		state = states[state]
+	except Exception as e:
+		logging.error(e)
+		logging.error('Failed to match state name to abbreviation. Input received ' + state)
+	return state
+
 def get_followers(uid):
 	followers = api.followers_ids(uid)
 	return followers
